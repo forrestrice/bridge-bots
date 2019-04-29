4fb7977195b06c5cc6fd6d5af20e58a518bda0e6
forrestrice <forrest.d.rice@gmail.com>
2019-04-28 22:44:01 -0700

    scrape all handrecords from acbl api

--- scrape/acbl_client.py
+++ scrape/acbl_client.py
@@ -1,5 +1,7 @@
 import json
+import logging
 import pathlib
+import time
 from pprint import pprint
 from requests import get
 
@@ -9,9 +11,31 @@ from requests import get
 
 class AcblClient:
     def __init__(self, token_file):
-        self.token = open("../acbl_token.txt", "r").read()
-        self.auth_headers = {"accept": "application/json", "Authorization": "Bearer " + token}
+        self.token = open(token_file, "r").read()
+        self.auth_headers = {"accept": "application/json", "Authorization": "Bearer " + self.token}
         self.base_url = "https://api.acbl.org/v1/"
+        self.logger = logging.getLogger("AcblClient")
 
-    def api_request(path, params):
-        return get(self.base_url + path, headers=self.auth_headers, params=params)
+    def api_request_json(self, path, params):
+        try:
+            return get(self.base_url + path, headers=self.auth_headers, params=params).json()
+        except Exception as ex:
+            self.logger.error("Exception fetching path=%s, params=%s: %s", path, params, ex)
+            raise ex
+
+
+    def _authorized_request_json(self, full_path):
+        return get(full_path, headers=self.auth_headers).json()
+
+    def join_all_pages_json(self, path, params):
+        all_data = []
+        first_resp_json = self.api_request_json(path, params)
+        all_data.extend(first_resp_json["data"])
+        next_page = first_resp_json["next_page_url"]
+        while next_page != None:
+            self.logger.debug("next_page: %s", next_page)
+            time.sleep(0.3)
+            resp_json = self._authorized_request_json(next_page)
+            all_data.extend(resp_json["data"])
+            next_page = resp_json["next_page_url"]
+        return all_data
---
+++ scrape/scrape_all_acbl.py
@@ -0,0 +1,95 @@
+import json
+import logging
+import pathlib
+import time
+
+from scrape.acbl_client import AcblClient
+
+ALL_EVENTS_FILENAME = "all_events.json"
+client = AcblClient("../acbl_token.txt")
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("AcblClient").setLevel(logging.DEBUG)
+
+# Download each session for the event
+def process_event(event_json, tournament_dir_path, tournament_sanction):
+    event_code = event_json["event_code"]
+    name = "_".join(event_json["name"].split()).replace("/", "-") + "_" + event_code
+    session_count = int(event_json["session_count"])
+    logging.info("\tevent %s", name)
+    event_dir_path = tournament_dir_path / name
+    event_dir_path.mkdir(parents=False, exist_ok=True)
+    for session_index in range(0, session_count):
+        session_num = session_index + 1
+        session_path = event_dir_path / f'session{session_num}'
+        if session_path.exists():
+            logging.info("\t\tskipping session %s", session_num)
+        else:
+            session_json = client.api_request_json("tournament/session",
+                                    {"sanction": tournament_sanction,
+                                     "event_code": event_code,
+                                     "session_number": session_num,
+                                     "with_handrecord": 1})
+
+            with open(event_dir_path / f'session{session_num}', "w") as session_file:
+                json.dump(session_json, session_file)
+            #sleep here so that we only slow down if we are hitting the api
+            time.sleep(0.3)
+
+
+def process_tournament(tournament_json):
+    sanction = tournament_json["sanction"]
+    name = "_".join(tournament_json["name"].split()).replace("/", "-") + "_" + sanction
+
+    logging.info("tournament %s", name)
+    tournament_dir_path = results_dir_path / name
+    tournament_dir_path.mkdir(parents=False, exist_ok=True)
+
+    # Download all tournament events and store them in all_events.json
+    all_events_path = tournament_dir_path / ALL_EVENTS_FILENAME
+    if not all_events_path.exists():
+        download_tournament_events(all_events_path, sanction)
+    else:
+        logging.info("%s already exists", ALL_EVENTS_FILENAME)
+
+    with open(all_events_path, "r") as all_events_file:
+        all_events = json.load(all_events_file)
+
+    for event in all_events:
+        process_event(event, tournament_dir_path, sanction)
+
+
+def download_tournament_events(all_events_path, sanction):
+    all_events_json = client.join_all_pages_json("tournament/event_query", {"sanction": sanction, "page_size": 50})
+    with open(all_events_path, "w") as all_events_file:
+        json.dump(all_events_json, all_events_file)
+
+
+
+results_dir_path = pathlib.Path("../results/acbl/")
+results_dir_path.mkdir(parents=True, exist_ok=True)
+results_path = results_dir_path / "all_tournaments.json"
+if not results_path.exists():
+
+    all_tournaments = client.join_all_pages_json("tournament_query", {"page_size": 50, "start_date": "2000-01-01"})
+    with open(results_path, "w") as tournament_file:
+        json.dump(all_tournaments, tournament_file)
+else:
+    logging.info("%s already exists. Will not re-fetch", results_path)
+
+logging.info("processing tournaments")
+
+with open(results_path, "r") as tournament_file:
+    all_tournaments = json.load(tournament_file)
+
+exception_count = 0
+for tournament in all_tournaments:
+    try:
+        process_tournament(tournament)
+    except Exception as ex:
+        exception_count += 1
+        logging.error("error processing tournament %s: %s",tournament["name"], ex)
+
+logging.info("encountered %s exceptions", exception_count)
+exit(exception_count)
+
