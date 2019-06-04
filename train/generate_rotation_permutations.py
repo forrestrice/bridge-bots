7893caa20c67341e172037ff4b98e3ea5c0f14d5
forrestrice <forrest.d.rice@gmail.com>
2019-06-04 00:43:42 -0700

    rotate deals and permutate non-trump suits for double dummy training

---
+++ train/generate_rotation_permutations.py
@@ -0,0 +1,36 @@
+import itertools
+from typing import List
+
+from deal.deal import Card
+from deal.deal_enums import BiddingSuit, Suit, Rank, Direction
+
+
+class RotationPermutation:
+    def __init__(self, suits: List[Suit]):
+        self.suits = suits
+        self.sorted_deck = [Card(suit, rank) for suit in suits for rank in Rank]
+        self.suit_ranks = {suit:suits.index(suit) for suit in suits}
+
+    def sort_hand(self, cards: List[Card]):
+        return sorted(cards, key=lambda card: (self.suit_ranks[card.suit], card.rank))
+
+    def build_deck_mask(self, cards: List[Card]):
+        deck_data = []
+        sorted_hand = self.sort_hand(cards)
+        sorted_hand_index = 0
+        for card in self.sorted_deck:
+            if sorted_hand_index < len(sorted_hand) and card == sorted_hand[sorted_hand_index]:
+                sorted_hand_index += 1
+                deck_data.append(1)
+            else:
+                deck_data.append(0)
+        return deck_data
+
+
+all_suit_permutations = [RotationPermutation(list(suit_permutation)) for suit_permutation in itertools.permutations([suit for suit in Suit])]
+
+clockwise_directions = [Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST]
+all_rotations = [[clockwise_directions[(i + j) % 4] for j in range(0, 4)] for i in range(0, 4)]
+
+RotationPermutation.all_suit_permutations = all_suit_permutations
+RotationPermutation.all_rotations = all_rotations
---
+++ train/prepare_rotated_first_suit_auto_ml_double_dummy_training.py
@@ -0,0 +1,68 @@
+import logging
+import pickle
+import random
+
+from deal.deal import Card
+from deal.deal_enums import Suit, Rank, Direction, BiddingSuit
+from train.generate_rotation_permutations import RotationPermutation
+from train.streaming_csv_writer import StreamingCsvWriter
+
+logging.basicConfig(level=logging.INFO)
+sorted_cards = sorted([Card(suit, rank) for suit in Suit for rank in Rank])
+card_headers = []
+for direction in Direction:
+    for card in sorted_cards:
+        card_headers.append(direction.name[0] + str(card.rank.value) + card.suit.name[0])
+
+csv_headers = ["split"] + card_headers + ["tricks"]
+
+suit_file_path = "./data/double_dummy/rotated_suit_auto_ml.csv"
+suit_training_writer = StreamingCsvWriter(suit_file_path)
+suit_training_writer.write_row(csv_headers)
+
+notrump_file_path = "./data/double_dummy/rotated_notrump_auto_ml.csv"
+notrump_training_writer = StreamingCsvWriter(notrump_file_path)
+notrump_training_writer.write_row(csv_headers)
+
+splits = ["TRAIN", "VALIDATE", "TEST"]
+split_weights = [0.8, 0.1, 0.1]
+
+
+def generate_deals():
+    with open("../results/double_dummy/all_deals.pickle", "rb") as dd_pickle_file:
+        while True:
+            try:
+                yield pickle.load(dd_pickle_file)
+            except EOFError:
+                break
+
+
+deal_count = 0
+for ddd in generate_deals():
+    deal_data = [random.choices(splits, split_weights)[0]]
+    for rotation in RotationPermutation.all_rotations:
+        score_direction = rotation[0]
+        for score_suit in BiddingSuit:
+            score = ddd.dd_score.scores[score_direction][score_suit]
+            if score_suit == BiddingSuit.NO_TRUMP:
+                trump_first_permutations = RotationPermutation.all_suit_permutations
+                row_writer = notrump_training_writer
+            else:
+                playing_suit = Suit(score_suit.value)
+                trump_first_permutations = [perm for perm in RotationPermutation.all_suit_permutations if
+                                            perm.suits[0] == playing_suit]
+                row_writer = suit_training_writer
+
+            for trump_first_permutation in trump_first_permutations:
+                row_data = deal_data.copy()
+                for rotation_direction in rotation:
+                    row_data.extend(trump_first_permutation.build_deck_mask(ddd.deal.player_cards[rotation_direction]))
+                row_data.append(score)
+                row_writer.write_row(row_data)
+
+    deal_count += 1
+    if deal_count % 1000 == 0:
+        logging.info("processed %s deals", deal_count)
+
+suit_training_writer.close()
+notrump_training_writer.close()
