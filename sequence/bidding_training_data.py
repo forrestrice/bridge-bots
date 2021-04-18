625608eed6a433bad92ffd4a726afe094ad06fff
Forrest Rice <frice@opentable.com>
2021-04-18 01:29:38 -0700

    v3 lstm model

--- sequence/bidding_training_data.py
+++ sequence/bidding_training_data.py
@@ -1,9 +1,71 @@
+import math
 from typing import List
 
+import tensorflow as tf
 import numpy as np
+from tensorflow.python.keras.utils.data_utils import Sequence
+
+from train.bridge_training_utils import bidding_vocab
 
 
 class BiddingTrainingData:
     def __init__(self, bidding_indices: List[int], holding_array: np.ndarray):
         self.bidding_indices = bidding_indices
         self.holding_array = holding_array
+
+
+class BiddingSequenceDataGenerator(Sequence):
+    def __init__(
+        self,
+        batch_size: int,
+        bidding_training_data: List[BiddingTrainingData],
+        bidding_sequence_length: int = 45,
+        shuffle_on_epoch_end: bool = True,
+        shuffle_on_start: bool = True,
+    ):
+        self.shuffle_on_epoch_end = shuffle_on_epoch_end
+        self.bidding_sequence_length = bidding_sequence_length
+        self.batch_size = batch_size
+        self.bidding_training_data = bidding_training_data
+        self.data_indices = np.arange(len(bidding_training_data))
+        self.bidding_vocab_size = len(bidding_vocab)
+        self.rnd = np.random.RandomState(0)
+        if shuffle_on_start:
+            self.rnd.shuffle(self.data_indices)
+
+    def __len__(self):
+        return math.ceil(len(self.bidding_training_data) / self.batch_size)
+
+    def __getitem__(self, batch_idx: int):
+        batch_indices = self.data_indices[batch_idx * self.batch_size : (batch_idx + 1) * self.batch_size]
+        batch_training_data = [self.bidding_training_data[i] for i in batch_indices]
+        batch_sequences = np.empty((0, self.bidding_sequence_length, self.bidding_vocab_size))
+        batch_targets = np.empty((0, self.bidding_vocab_size))
+        batch_holdings = np.empty((0, 52 * 4))
+        for single_training_data in batch_training_data:
+            sequences, targets = self.generate_sequences_and_targets(single_training_data)
+            batch_sequences = np.concatenate((batch_sequences, sequences), axis=0)
+            batch_targets = np.concatenate((batch_targets, targets), axis=0)
+            tiled_holdings = np.tile(single_training_data.holding_array, (len(sequences), 1))
+            batch_holdings = np.concatenate((batch_holdings, tiled_holdings), axis=0)
+
+        return [batch_sequences, batch_holdings], batch_targets
+
+    def on_epoch_end(self) -> None:
+        """ Reshuffle data on epoch end """
+        if self.shuffle_on_epoch_end:
+            self.rnd.shuffle(self.data_indices)
+
+    def generate_sequences_and_targets(self, data_sample: BiddingTrainingData):
+        sequences = []
+        targets = []
+        for i in range(len(data_sample.bidding_indices)):
+            sequences.append(data_sample.bidding_indices[0:i])
+            targets.append(data_sample.bidding_indices[i])
+
+        padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(
+            sequences, padding="pre", maxlen=self.bidding_sequence_length, truncating="post"
+        )
+        one_hot_bidding = tf.keras.utils.to_categorical(padded_sequences, num_classes=self.bidding_vocab_size)
+        one_hot_targets = tf.keras.utils.to_categorical(targets, num_classes=self.bidding_vocab_size)
+        return one_hot_bidding, one_hot_targets
---
+++ sequence/evaluate_bid_four_hand.py
@@ -0,0 +1,20 @@
+import pickle
+from typing import List
+import tensorflow as tf
+from tensorflow import keras
+
+
+from sequence.bidding_training_data import BiddingSequenceDataGenerator, BiddingTrainingData
+
+bid_learn_prefix = "/Users/frice/bridge/bid_learn/"
+
+
+lstm_model = keras.models.load_model(bid_learn_prefix + "lstm_model_v3")
+
+with open(bid_learn_prefix + "VALIDATION.pickle", "rb") as pickle_file:
+    bidding_validation_data: List[BiddingTrainingData] = pickle.load(pickle_file)
+
+data_generator = BiddingSequenceDataGenerator(20, bidding_validation_data)
+
+lstm_model.evaluate(data_generator)
+
